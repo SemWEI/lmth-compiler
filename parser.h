f1074408e231f5a7c0cec3bcb8c08dcdd8e7fe24
Anupama Gamage <anupamag@alliontechnologies.com>
2024-01-10 19:14:00 +0530

    Added Parser

---
+++ parser.h
@@ -0,0 +1,9 @@
+#include "parser.h"
+
+ASTNode Parser::parse(const std::vector<Token> &tokens) {
+    ASTNode root;
+
+    // Implement parsing logic to build the AST from tokens
+
+    return root;
+}
